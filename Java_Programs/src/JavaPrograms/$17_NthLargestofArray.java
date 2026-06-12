2f8437aaa84cd2ac2e269df42973d4c17da9bcc9
RenoldMalhare <119624133+RenoldMalhare@users.noreply.github.com>
2026-06-12 10:02:04 +0530

    java

--- Java_Programs/src/JavaPrograms/$17_NthLargestofArray.java
+++ Java_Programs/src/JavaPrograms/$17_NthLargestofArray.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class $17_NthLargestofArray {
 	public static void main(String[] args) {
 
-		int[] ar = { 3, 4, 5, 6, 7, 2, 6, 7 };
-		int n = 3;
+		int[] ar = { 3, 4, 5, 6, 4, 3,7, 2, 6, 7 };
+		int n = 5;
 		int count = 0;
 		Arrays.sort(ar);
+		///34456677
 
 		for (int i = ar.length - 1; i > 0; i--) {
 			if (ar[i] > ar[i - 1]) {
@@ -16,6 +17,7 @@ public class $17_NthLargestofArray {
 			}
 			if (count == n) {
 				System.out.println(ar[i]);
+				break;
 			}
 
 		}
